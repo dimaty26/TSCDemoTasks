982cb3f5fc478f280f1069f8a9aad94caa5f0d3a
dimaty26 <dimaty26rus@mail.ru>
2020-01-24 11:40:49 +0300

    refresh

--- FirstTask/src/main/java/com/zmeev/TransferProcessor.java
+++ FirstTask/src/main/java/com/zmeev/TransferProcessor.java
@@ -6,10 +6,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class TransferProcessor {
-    private static List<String> message = new ArrayList<>();
+class TransferProcessor {
+    private static List<String> messages = new ArrayList<>();
 
-    public static List<String> getPossibleTransfer(List<Employee> employees) {
+    static List<String> getPossibleTransfer(List<Employee> employees) {
 
         Map<Department, List<Employee>> groupedByDeptMap = Department.getGroupedByDeptMap(employees);
 
@@ -18,63 +18,59 @@ public class TransferProcessor {
             List<Employee> value1 = entry1.getValue();
             for (Map.Entry<Department, List<Employee>> entry2 : groupedByDeptMap.entrySet()) {
                 Department key2 = entry2.getKey();
-                if (key1.compareTo(key2) >= 0) continue;
+                if (key1.compareTo(key2) == 0) continue;
                 List<Employee> value2 = entry2.getValue();
-                message.add(String
-                        .format("Check possible transfer between %s and %s departments:",
-                                value1.get(0).getDepartment(),
-                                value2.get(0).getDepartment()));
                 getPossibleTransferBtwnDepartments(value1, value2);
             }
         }
-        return message;
+        return messages;
     }
 
-    public static void getPossibleTransferBtwnDepartments(List<Employee> employees1, List<Employee> employees2) {
+    private static void getPossibleTransferBtwnDepartments(List<Employee> employees1, List<Employee> employees2) {
         int r = employees1.size() - 1;
 
         while (r > 0) {
             printCombination(employees1, employees2, r);
-            //printCombination(employees2, employees1, r);
             r--;
         }
     }
 
-    private static void combinationUtil(List<Employee> employees, List<Employee> tempEmployees,
-                                        List<Employee> employees2, int start, int end, int index, int r,
-                                        BigDecimal wage1, BigDecimal wage2) {
-        StringBuffer sb = new StringBuffer();
+    private static void employeesCombinationUtil(List<Employee> employees1, List<Employee> tempEmployees,
+                                                 List<Employee> employees2, int start, int end, int index, int r,
+                                                 BigDecimal wage1, BigDecimal wage2) {
+        BigDecimal newAvgWage1;
+        BigDecimal newAvgWage2;
+        StringBuilder sb;
 
-        List<Employee> e1Copied = employees.stream().map(Employee::clone).collect(Collectors.toList());
+        List<Employee> e1Copied = employees1.stream().map(Employee::clone).collect(Collectors.toList());
         List<Employee> e2Copied = employees2.stream().map(Employee::clone).collect(Collectors.toList());
 
         if (index == r) {
+            sb = new StringBuilder();
             e1Copied.removeAll(tempEmployees);
             e2Copied.addAll(tempEmployees);
-            BigDecimal NewAvgWage1;
-            BigDecimal NewAvgWage2;
-            if ((NewAvgWage1 = Employee.getAverageWageAmongEmployees(e1Copied)).compareTo(wage1) > 0
-                    && (NewAvgWage2 = Employee.getAverageWageAmongEmployees(e2Copied)).compareTo(wage2) > 0) {
+            if ((newAvgWage1 = Employee.getAverageWageAmongEmployees(e1Copied)).compareTo(wage1) > 0
+                    && (newAvgWage2 = Employee.getAverageWageAmongEmployees(e2Copied)).compareTo(wage2) > 0) {
                 if (tempEmployees.size() != 1) {
                     for (Employee e : tempEmployees) {
                         sb.append(e.getName()).append(", ");
                     }
-                    message.add(sb.toString().trim().substring(0, sb.length() - 2) + " could be transferred from department " +
+                    messages.add(sb.toString().trim().substring(0, sb.length() - 2) + " could be transferred from department " +
                             e1Copied.get(0).getDepartment() + " to department " +
                             e2Copied.get(0).getDepartment());
                 } else {
                     for (Employee e : tempEmployees) {
-                        message.add(e.getName() + " could be transferred from department " +
+                        messages.add(e.getName() + " could be transferred from department " +
                                 e.getDepartment() + " to department " +
                                 e2Copied.get(0).getDepartment());
                     }
                 }
-                message.add(String.format("Average wage in department %s will increase to " + NewAvgWage2
-                                + "\nAverage wage in department %s will increase to " + NewAvgWage1,
+                messages.add(String.format("Average wage in department %s will increase to " + newAvgWage2
+                                + "\nAverage wage in department %s will increase to " + newAvgWage1,
                         e1Copied.get(0).getDepartment(),
                         e2Copied.get(0).getDepartment()));
             }
-            return;
+            return ;
         }
 
         // replace index with all possible elements. The condition
@@ -82,12 +78,12 @@ public class TransferProcessor {
         // at index will make a combination with remaining elements
         // at remaining positions
         for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
-            tempEmployees.set(index, employees.get(i));
-            combinationUtil(employees, tempEmployees, employees2, i + 1, end, index + 1, r, wage1, wage2);
+            tempEmployees.set(index, employees1.get(i));
+            employeesCombinationUtil(employees1, tempEmployees, employees2, i + 1, end, index + 1, r, wage1, wage2);
         }
     }
 
-    static void printCombination(List<Employee> employees1, List<Employee> employees2, int r) {
+    private static void printCombination(List<Employee> employees1, List<Employee> employees2, int r) {
         BigDecimal wage1 = Employee.getAverageWageAmongEmployees(employees1);
         BigDecimal wage2 = Employee.getAverageWageAmongEmployees(employees2);
 
@@ -97,6 +93,6 @@ public class TransferProcessor {
         for (int i = 0; i < r; i++) {
             data.add(null);
         }
-        combinationUtil(employees1, data, employees2, 0, n-1, 0, r, wage1, wage2);
+        employeesCombinationUtil(employees1, data, employees2, 0, n-1, 0, r, wage1, wage2);
     }
 }
